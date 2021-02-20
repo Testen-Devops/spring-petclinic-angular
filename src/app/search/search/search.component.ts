ef1716e1af56616939e78011915bdd4f44c20983
Jan <bolz_j@web.de>
2021-02-20 11:11:26 +0100

    error handling

--- src/app/search/search/search.component.ts
+++ src/app/search/search/search.component.ts
@@ -23,21 +23,27 @@ export class SearchComponent implements OnInit {
 
   ngOnInit() {
     this.ownerService.getOwnerByKey(this.input).subscribe(owners => {
-      const type = "owners";
+      const type = "Owner";
       for (let i = 0; i < owners.length; i++) {
         let name = owners[i].firstName + " " + owners[i].lastName;
         let result = new Result(name, type, owners[i].id);
         this.results.push(result);
       }
+    },
+    (error) => {
+     console.log('ERR: No Owners found')
     });
 
     this.petService.getPetsByKey(this.input).subscribe(pets => {
-      const type = "pets";
+      const type = "Pet";
       for (let i = 0; i < pets.length; i++) {
-        let name = pets[i].name + ",   Owner: " + pets[i].owner.firstName + " " + pets[i].owner.lastName;
-        let result = new Result(name, type, pets[i].id);
+        let name = pets[i].name + ", " + pets[i].type.name + " Owner: " + pets[i].owner.firstName + " " + pets[i].owner.lastName;
+        let result = new Result(name, type, pets[i].owner.id);
         this.results.push(result);
       }
+    },
+    (error) => {
+      console.log('ERR: No Pets found')
     });
   }
 
