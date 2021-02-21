47c9065dcf3e8e74857c0e4ad2217ba8a6df3852
Lukas Karsten <l.karsten@ce-con.de>
2021-02-21 19:28:33 +0100

    deleted feature

--- cypress/integration/delete_owner.spec.js
+++ cypress/integration/delete_owner.spec.js
@@ -1,20 +1,32 @@
-it('deletes owner', () => {
-  cy.visit('/');
-  cy.contains('Owners').click();
-  cy.contains('All').click();
+  it('deletes owner', () => {
+    cy.visit('/');
+    cy.contains('Owners')
+      .click();
+    cy.contains('All')
+      .click();
 
-  var ownersName;
-  cy.get('td a').first().then(($firstOwner) => {
+    // save owners name for comparison
+    var ownersName;
+    cy.get('td a')
+      .first()
+      .then(($firstOwner) => {
+        ownersName = $firstOwner.text();  
+        })
+        .click()
 
-    ownersName = $firstOwner.text();  
-  }).click()
+    cy.contains('Delete Owner')
+      .click();
 
-  cy.contains('Delete Owner').click();
+    cy.url() 
+      .should('equal', Cypress.config().baseUrl+'/petclinic/owners') 
 
-  cy.url().should('equal', Cypress.config().baseUrl+'/petclinic/owners') 
+    // check if deleted owner is not in list anymore
+    cy.get('td a')
+      .first()
+        .then(($nextOwner) => {
+          expect($nextOwner
+          .text())
+          .to.not.equal(ownersName)
+        })
 
-  cy.get('td a').first().then(($nextOwner) => {
-    expect($nextOwner.text()).to.not.equal(ownersName)
-  })
-
-});
+  });
