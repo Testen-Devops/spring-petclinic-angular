8276253e2a81b9011d7687e6b3e6cdfcab58df4d
Lukas Karsten <l.karsten@ce-con.de>
2021-02-21 14:27:53 +0100

    unit tests for petService.getPetsByKey

--- src/app/pets/pet.service.spec.ts
+++ src/app/pets/pet.service.spec.ts
@@ -27,17 +27,65 @@ import {async, inject, TestBed} from '@angular/core/testing';
 import {PetService} from './pet.service';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import {HttpClient} from '@angular/common/http';
+import { HttpErrorHandler } from 'app/error.service';
+import { Type } from "@angular/core";
+import { Pet } from './pet';
 
-describe('PetService', () => {
+
+fdescribe('PetService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let petService: PetService;
   beforeEach(() => {
     TestBed.configureTestingModule({
       // Import the HttpClient mocking services
       imports: [HttpClientTestingModule],
-      providers: [PetService]
+      providers: [PetService,HttpErrorHandler]
+    });
+    // Inject the http, test controller, and service-under-test
+    // as they will be referenced by each test.
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get<HttpTestingController>(
+      HttpTestingController as Type<HttpTestingController>
+    );
+      petService = TestBed.get(PetService);
+    });
+
+    // id: number;
+    // name: string;
+    // birthDate: string;
+    // type: PetType;
+    // owner: Owner;
+    // visits: Visit[];
+
+    let expectedPets: Pet[];
+    beforeEach(() => {
+      petService = TestBed.get(PetService);
+      expectedPets = [
+        { id: 1, name: "A",birthDate:"09/08/1994" },
+        { id: 2, name: "A 2",birthDate:"23/08/2000" },
+        { id: 3, name: "A 4",birthDate:"23/08/2000" },
+      ] as Pet[];
+    });
+
+    it("should return pets by key", () => {
+      petService
+        .getPetsByKey('A')
+        .subscribe(
+          (owners) =>
+            expect(owners).toEqual(
+              expectedPets,
+              "should return expected pets"
+            ),
+          fail
+        );
+
+      // OwnerService should have made one request to GET owners from expected URL
+      const req = httpTestingController.expectOne(petService.entityUrl + '/search/A');
+      expect(req.request.method).toEqual("GET");
+
+      // Respond with the mock owners
+      req.flush(expectedPets);
     });
   });
 
-  it('should ...', async(inject([HttpTestingController], (petService: PetService, http: HttpClient) => {
-    expect(petService).toBeTruthy();
-  })));
-});
