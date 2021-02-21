03dd04f8736fd225073089bbb63094f70bac6a23
Nino Peters <dev@ninopeters.de>
2021-02-21 09:32:58 +0100

    fix already implemented unit tests

--- src/app/visits/visit-list/visit-list.component.spec.ts
+++ src/app/visits/visit-list/visit-list.component.spec.ts
@@ -22,18 +22,19 @@
  * @author Vitaliy Fedoriv
  */
 
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 
-import {VisitListComponent} from './visit-list.component';
-import {FormsModule} from '@angular/forms';
-import {VisitService} from '../visit.service';
-import {ActivatedRoute, Router} from '@angular/router';
-import {ActivatedRouteStub, RouterStub} from '../../testing/router-stubs';
-import {Visit} from '../visit';
-import {Pet} from '../../pets/pet';
-import {Observable, of} from 'rxjs';
+import { VisitListComponent } from "./visit-list.component";
+import { FormsModule } from "@angular/forms";
+import { VisitService } from "../visit.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ActivatedRouteStub, RouterStub } from "../../testing/router-stubs";
+import { Visit } from "../visit";
+import { Pet } from "../../pets/pet";
+import { Observable, of } from "rxjs";
 import Spy = jasmine.Spy;
+import { Vet } from "app/vets/vet";
 
 class VisitServiceStub {
   deleteVisit(visitId: string): Observable<number> {
@@ -41,13 +42,14 @@ class VisitServiceStub {
   }
 }
 
-describe('VisitListComponent', () => {
+describe("VisitListComponent", () => {
   let component: VisitListComponent;
   let fixture: ComponentFixture<VisitListComponent>;
   let visitService: VisitService;
   let testVisits: Visit[];
   let testPet: Pet;
   let spy: Spy;
+  let testVet: Vet;
   let responseStatus: number;
 
   beforeEach(async(() => {
@@ -56,12 +58,11 @@ describe('VisitListComponent', () => {
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       imports: [FormsModule],
       providers: [
-        {provide: VisitService, useClass: VisitServiceStub},
-        {provide: Router, useClass: RouterStub},
-        {provide: ActivatedRoute, useClass: ActivatedRouteStub}
-      ]
-    })
-      .compileComponents();
+        { provide: VisitService, useClass: VisitServiceStub },
+        { provide: Router, useClass: RouterStub },
+        { provide: ActivatedRoute, useClass: ActivatedRouteStub },
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -69,45 +70,55 @@ describe('VisitListComponent', () => {
     component = fixture.componentInstance;
     testPet = {
       id: 1,
-      name: 'Leo',
-      birthDate: '2010-09-07',
-      type: {id: 1, name: 'cat'},
+      name: "Leo",
+      birthDate: "2010-09-07",
+      type: { id: 1, name: "cat" },
       owner: {
         id: 1,
-        firstName: 'George',
-        lastName: 'Franklin',
-        address: '110 W. Liberty St.',
-        city: 'Madison',
-        telephone: '6085551023',
-        pets: null
+        firstName: "George",
+        lastName: "Franklin",
+        address: "110 W. Liberty St.",
+        city: "Madison",
+        telephone: "6085551023",
+        pets: null,
       },
-      visits: null
+      visits: null,
     };
-    testVisits =  [{
+    testVet = {
       id: 1,
-      date: '2016-09-07',
-      description: '',
-      pet: testPet
-    }];
+      firstName: "Test",
+      lastName: "Test",
+      specialties: [],
+      visits: [],
+    };
+    testVisits = [
+      {
+        id: 1,
+        date: "2016-09-07",
+        description: "",
+        pet: testPet,
+        vet: testVet,
+      },
+    ];
 
     visitService = fixture.debugElement.injector.get(VisitService);
     responseStatus = 204; // success delete return NO_CONTENT
     component.visits = testVisits;
 
-    spy = spyOn(visitService, 'deleteVisit')
-      .and.returnValue(of(responseStatus));
+    spy = spyOn(visitService, "deleteVisit").and.returnValue(
+      of(responseStatus)
+    );
 
     fixture.detectChanges();
   });
 
-  it('should create VisitListComponent', () => {
+  it("should create VisitListComponent", () => {
     expect(component).toBeTruthy();
   });
 
-  it('should call deleteVisit() method', () => {
+  it("should call deleteVisit() method", () => {
     fixture.detectChanges();
     component.deleteVisit(component.visits[0]);
-    expect(spy.calls.any()).toBe(true, 'deleteVisit called');
+    expect(spy.calls.any()).toBe(true, "deleteVisit called");
   });
-
 });
